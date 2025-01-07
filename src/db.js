b0216e6a2c0a9a743bbffb30fc0ec410bde8e57c
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2025-01-07 18:37:28 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
   try {
-    console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI);
+    //console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Successfully connected to MongoDB!');
   } catch (error) {
